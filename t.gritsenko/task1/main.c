5d317ff7b38bcc0b184709fd14d80fd2b73b6b9e
LijnxArcher <lynx20030311@gmail.com>
2025-10-08 20:52:36 +0700

    task 1 fixed

--- t.gritsenko/task1/main.c
+++ t.gritsenko/task1/main.c
@@ -89,8 +89,7 @@ int main(int argc, char *argv[]) {
 			break;
 
 		case 'u':
-			long max_processes = sysconf(_SC_CHILD_MAX);
-        	printf("Max child processes per user: %ld\n", max_processes);
+        	printf("Max child processes per user: %ld\n", sysconf(_SC_CHILD_MAX););
 			break;
 
 		case 'U': {
