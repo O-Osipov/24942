fe1e6c287b970e9b6878dd64dbedb4ba17da1d1b
Nexxa <an_na_rib@mail.ru>
2025-10-22 11:29:29 +0700

    task17 test

---
+++ a.rybina/task17/task17.c
@@ -0,0 +1,116 @@
+// Многие программы, принимающие ввод с терминала, позволяют редактировать строку перед использованием. Напишите программу, которая выключает эхо и каноническую обработку, таким образом выключив и обработку символа забоя. Ваша программа должна получать ввод с клавиатуры и показывать его на терминале в соответствии со следующими правилами:
+//      Каждый введенный символ должен немедленно появляться на дисплее.
+//      Когда вводится символ ERASE, стирается последний символ в текущей строке.
+//      Когда вводится символ KILL, стираются все символы в текущей строке.
+//      Когда вводится CTRL-W, стирается последнее слово в текущей строке, вместе со всеми следующими за ним пробелами.
+//      Программа завершается, когда введен CTRL-D и курсор находится в начале строки.
+//      Все непечатаемые символы, кроме перечисленных выше, должны издавать звуковой сигнал, выводя на терминал символ CTRL-G.
+//      Длина строки ограничена 40 символами. Если какое-то слово пересекает 40-й столбец, это слово должно быть помещено в начало следующей строки.
+
+#include <unistd.h>
+#include <sys/termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <sys/ttydefaults.h>
+#include <string.h>
+
+#define LINE_LENGTH 40
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // Terminal control set attribute
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode); // At exit do this
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+    enableRawMode();
+
+    char c;
+    static char line[LINE_LENGTH + 1];
+    int len = 0;  // Track current line length
+
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (iscntrl(c) || !isprint(c)) { // Control key or is printable
+            switch (c) {
+            case CERASE: { // backspace
+                if (len > 0) {
+                    line[len - 1] = '\0';
+                    len--;
+                    printf("\33[D\33[K");  // Move left and clear to end of line
+                }
+                break;
+            }
+
+            case CKILL: { // ctrl+U
+                line[0] = '\0';
+                len = 0;
+                printf("\33[2K\r");  // Clear entire line and move to beginning
+                break;
+            }
+
+            case CWERASE: { // ctrl+W
+                if (len > 0) {
+                    // Find start of last word
+                    int word_start = 0;
+                    char prev = ' ';
+                    for (int i = 0; i < len; i++) {
+                        if (line[i] != ' ' && prev == ' ') {
+                            word_start = i;
+                        }
+                        prev = line[i];
+                    }
+
+                    // Erase from word_start to end
+                    int chars_to_erase = len - word_start;
+                    line[word_start] = '\0';
+                    len = word_start;
+
+                    // Move cursor back and clear
+                    printf("\33[%dD\33[K", chars_to_erase);
+                }
+                break;
+            }
+
+            case CEOF: { // ctrl+D
+                if (len == 0) { 
+                    exit(0); 
+                }
+                break;
+            }
+
+            default: {
+                putchar('\a');  // Bell character for unhandled control chars
+                break;
+            }
+            }
+        }
+        else {
+            // Printable character
+            if (len == LINE_LENGTH) {
+                putchar('\n');
+                len = 0;
+                line[0] = '\0';
+            }
+
+            if (len < LINE_LENGTH) {
+                line[len++] = c;
+                line[len] = '\0';
+                putchar(c);
+            }
+        }
+
+        fflush(stdout);
+    }
+
+    return 0;
+}
+
