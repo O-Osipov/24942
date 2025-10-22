738c21537a5604368aaa78f7367eb0323e54f0f1
Nexxa <an_na_rib@mail.ru>
2025-10-22 16:16:48 +0700

    '

--- a.rybina/task17/task17.c
+++ a.rybina/task17/task17.c
@@ -1,3 +1,12 @@
+// Многие программы, принимающие ввод с терминала, позволяют редактировать строку перед использованием. Напишите программу, которая выключает эхо и каноническую обработку, таким образом выключив и обработку символа забоя. Ваша программа должна получать ввод с клавиатуры и показывать его на терминале в соответствии со следующими правилами:
+//      Каждый введенный символ должен немедленно появляться на дисплее.
+//      Когда вводится символ ERASE, стирается последний символ в текущей строке.
+//      Когда вводится символ KILL, стираются все символы в текущей строке.
+//      Когда вводится CTRL-W, стирается последнее слово в текущей строке, вместе со всеми следующими за ним пробелами.
+//      Программа завершается, когда введен CTRL-D и курсор находится в начале строки.
+//      Все непечатаемые символы, кроме перечисленных выше, должны издавать звуковой сигнал, выводя на терминал символ CTRL-G.
+//      Длина строки ограничена 40 символами. Если какое-то слово пересекает 40-й столбец, это слово должно быть помещено в начало следующей строки.
+
 #include <unistd.h>
 #include <sys/termios.h>
 #include <stdlib.h>
@@ -6,6 +15,7 @@
 #include <string.h>
 
 #define LINE_LENGTH 40
+
 // Define control characters for Solaris compatibility
 #ifndef CERASE
 #define CERASE '\b'    // Backspace
@@ -22,12 +32,12 @@
 struct termios orig_termios;
 
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // Terminal control set attribute
 }
 
 void enableRawMode() {
     tcgetattr(STDIN_FILENO, &orig_termios);
-    atexit(disableRawMode);
+    atexit(disableRawMode); // At exit do this
 
     struct termios raw = orig_termios;
     raw.c_lflag &= ~(ECHO | ICANON);
@@ -39,73 +49,81 @@ int main() {
 
     char c;
     static char line[LINE_LENGTH + 1];
+    int len = 0;  // Track current line length
 
     while (read(STDIN_FILENO, &c, 1) == 1) {
-        int len = strlen(line);
-        if (iscntrl(c) || !isprint(c)) {
+        if (iscntrl(c) || !isprint(c)) { // Control key or is printable
             switch (c) {
-            case CERASE: {
-                line[len - 1] = 0;
-
-                printf("\33[D\33[K");
-
-                break;
-            }
-
-            case CKILL: {
-
-                line[0] = 0;
-
-                printf("\33[2K\r");
-
-                break;
-            }
-
-            case CWERASE: {
-
-                int word_start = 0;
-                char prev = ' ';
-                for (int i = 0; i < len; i++) {
-                    if (line[i] != ' ' && prev == ' ') {
-                        word_start = i;
-                    }
-                    prev = line[i];
+            case CERASE: { // backspace
+                if (len > 0) {
+                    line[len - 1] = '\0';
+                    len--;
+                    printf("\33[D\33[K");  // Move left and clear to end of line
                 }
-
-                line[word_start] = 0;
-
-               printf("\33[%dD\33[K", len - word_start);
-
                 break;
             }
 
-            case CEOF: {
-               
-                if (line[0] == 0) { exit(0); }
+            case CKILL: { // ctrl+U
+                line[0] = '\0';
+                len = 0;
+                printf("\33[2K\r");  // Clear entire line and move to beginning
+                break;
+            }
+
+            case CWERASE: { // ctrl+W
+                if (len > 0) {
+                    // Find start of last word
+                    int word_start = 0;
+                    char prev = ' ';
+                    for (int i = 0; i < len; i++) {
+                        if (line[i] != ' ' && prev == ' ') {
+                            word_start = i;
+                        }
+                        prev = line[i];
+                    }
+
+                    // Erase from word_start to end
+                    int chars_to_erase = len - word_start;
+                    line[word_start] = '\0';
+                    len = word_start;
+
+                    // Move cursor back and clear
+                    printf("\33[%dD\33[K", chars_to_erase);
+                }
+                break;
+            }
+
+            case CEOF: { // ctrl+D
+                if (len == 0) { 
+                    exit(0); 
+                }
                 break;
             }
 
             default: {
-               
-                putchar('\a');
+                putchar('\a');  // Bell character for unhandled control chars
                 break;
             }
             }
         }
         else {
+            // Printable character
             if (len == LINE_LENGTH) {
                 putchar('\n');
                 len = 0;
+                line[0] = '\0';
             }
 
-            line[len++] = c;
-            line[len] = 0;
-
-            putchar(c);
+            if (len < LINE_LENGTH) {
+                line[len++] = c;
+                line[len] = '\0';
+                putchar(c);
+            }
         }
 
-        fflush(NULL);
+        fflush(stdout);
     }
 
     return 0;
 }
+
