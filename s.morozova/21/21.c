fb5a004c8904becc29cf137869ae289f67177b80
Soffa <Solmkey@gmail.com>
2025-11-05 14:57:23 +0700

    ex21_1

--- s.morozova/21/21.c
+++ s.morozova/21/21.c
@@ -5,13 +5,27 @@
 
 volatile sig_atomic_t count = 0;
 
-void handle_sigint(int sig) { count++; write(1, "\a", 1); }
-void handle_sigquit(int sig) { printf("\n%d\n", count); exit(0); }
+void handle_sigint(int sig) { 
+    count++; 
+    write(1, "\a", 1); 
+    // Повторно устанавливаем обработчик
+    signal(SIGINT, handle_sigint);
+}
+
+void handle_sigquit(int sig) { 
+    printf("\n%d\n", count); 
+    exit(0); 
+}
 
 int main() {
     signal(SIGINT, handle_sigint);
     signal(SIGQUIT, handle_sigquit);
     
-    while (1) pause();
+    printf("Программа запущена. Ctrl-C для звука, Ctrl-\\ для выхода\n");
+    
+    while (1) {
+        pause();
+    }
+    
     return 0;
 }
