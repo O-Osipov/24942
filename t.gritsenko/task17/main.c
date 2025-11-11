2c7358b4771892116f35d4321e7f6dbd0855e833
LijnxArcher <lynx20030311@gmail.com>
2025-11-12 00:39:29 +0700

    tasks 9, 10, 17 done

---
+++ t.gritsenko/task17/main.c
@@ -0,0 +1,126 @@
+#include <unistd.h>
+#include <sys/termios.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <sys/ttydefaults.h>
+#include <string.h>
+
+#define LINE_LENGTH 40
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);  //set old terminal settings
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);     //remember default termios
+    atexit(disableRawMode);                     //call on exit
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);            //disable echo and canon mode
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);   //set new terminal settings
+}
+
+int main() {
+    enableRawMode();
+
+    char c;
+    static char line[LINE_LENGTH + 1];
+
+     while (read(STDIN_FILENO, &c, 1) == 1) {
+        int len = strlen(line);
+        if (iscntrl(c) || !isprint(c)) {
+            switch (c) {
+                case CERASE: {
+                    // ERASE - стереть последний симовол в строке.
+
+                    line[len - 1] = 0;
+
+                    // [D - двигает курсор на знак влево
+                    // [K - чистит строку справа от курсора
+                    // printf("\33[D\33[K");
+                    
+                    printf("\b \b");
+
+                    break;
+                }
+
+                case CKILL: {
+                    // KILL - стереть все символы в строке.
+                    line[0] = 0;
+
+                    // [2K - чистит строку целиком
+                    printf("\33[2K\r");
+
+                    break;
+                }
+
+                case CWERASE: {
+                    // CTRL-W - стереть последнее слово в строке
+
+                    int word_start = 0;
+                    char prev = ' ';
+                    for (int i = 0; i < len; i++) {
+                        if (line[i] != ' ' && prev == ' ') {
+                            word_start = i;
+                        }
+                        prev = line[i];
+                    }
+                    line[word_start] = 0;
+
+                    // [<n>D - двигает курсор на n знаков влево
+                    // [K - чистит строку справа от курсора
+                    printf("\33[%dD\33[K", len - word_start);
+
+                    break;
+                }
+
+                case CEOF: {
+                    // CTRL-D - завершение программы, если
+                    // курсор находится в начале строки
+
+                    if (line[0] == 0) { exit(0); }
+                    break;
+                }
+
+                default: {
+                    // Все остальные непечатаемые символы должны
+                    // издавать звуковой сигнал (CTRL-G)
+                    putchar('\a');
+                    break;
+                }
+            }
+        } else {
+            if (len == LINE_LENGTH) {
+
+                int i = len - 1;
+                while (i >= 0 && line[i] != ' ') i--;
+                int word_start = i + 1;
+                
+                // Перенос слова на новую строчку
+                if (word_start != 0  && word_start != 40) {
+                    printf("\33[%dD\33[K", len - word_start);
+                    putchar('\n');
+                    printf(line + word_start);
+
+                    memmove(line, line + word_start, len - word_start);
+                    len = len - word_start;
+                } else {
+                    putchar('\n');
+                    len = 0;
+                }
+
+            }
+
+            line[len++] = c;
+            line[len] = 0;
+
+            putchar(c);
+        }
+
+        fflush(NULL);
+    }
+
+    return 0;
+}
