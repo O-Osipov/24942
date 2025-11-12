70949bc14c4f5f0cb854bea212c7d190f856eafe
Erbol Kubanychbek <e.kubanychbek@g.nsu.ru>
2025-11-12 16:41:40 +0700

    21.c

--- e.kubanychbek/task21/21.c
+++ e.kubanychbek/task21/21.c
@@ -4,7 +4,7 @@
 
 static volatile sig_atomic_t cnt = 0, quit = 0;
 
-void h_int(int s)   { (void)s; cnt++; write(1, "\a", 1); }
+void h_int(int s)   { (void)s; cnt++; printf("\a"); }
 void h_quit(int s)  { (void)s; quit = 1; }
 
 int main(void) {
@@ -12,6 +12,6 @@ int main(void) {
     sigset(SIGQUIT, h_quit);
     for (;;){
         pause();
-        if (quit) { printf("Звук прозвучал %d раз(а)\n", cnt); return 0; }
+        if (quit) { printf("написано %d раз(а)\n", cnt); return 0; }
     }
 }
