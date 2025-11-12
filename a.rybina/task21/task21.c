886e8c7c9fefd8dc206d900b0c127905f920b9d7
Nexxa <an_na_rib@mail.ru>
2025-11-12 16:00:44 +0700

    removed windows adaptation by double pressing ctrl+c due to incorrect working on Unix

--- a.rybina/task21/task21.c
+++ a.rybina/task21/task21.c
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <time.h>
 
 // For Windows compatibility
 #ifndef SIGQUIT
@@ -12,28 +11,14 @@
 #endif
 
 int count = 0;
-time_t last_sigint_time = 0;
-const int DOUBLE_PRESS_WINDOW = 1; // seconds
 
 void handleSIGINT(int sig) {
     (void)sig;  // Suppress unused parameter warning
-    time_t current_time = time(NULL);
-
-    // Make sound and increment count first (for both single and double press)
     printf("\a");  // Make sound
     fflush(stdout);
     count++;
     printf(" [Sound #%d] ", count);  // Debug output
     fflush(stdout);
-
-    // Check if this is a double press (within 1 second of last press)
-    if (last_sigint_time != 0 && (current_time - last_sigint_time) < DOUBLE_PRESS_WINDOW) {
-        printf("\nThe signal sounded %d times.\n", count);
-        exit(0);
-    }
-
-    last_sigint_time = current_time;
-
     // Re-register the signal handler (some systems reset it after first call)
     signal(SIGINT, handleSIGINT);
 }
@@ -49,7 +34,6 @@ int main() {
     signal(SIGQUIT, handleSIGQUIT);
     
     printf("Program started. Press CTRL-C to make sound, CTRL-\\ to quit.\n");
-    printf("On Windows: Press CTRL-C twice quickly to quit.\n");
     printf("Sound count: %d\n", count);
     
     while (1) {
