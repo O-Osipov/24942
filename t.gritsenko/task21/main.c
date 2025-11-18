f549cc3798de811136fe86b864133edc3377314e
LijnxArcher <lynx20030311@gmail.com>
2025-11-19 03:37:00 +0700

    task21 test fix

--- t.gritsenko/task21/main.c
+++ t.gritsenko/task21/main.c
@@ -7,19 +7,30 @@ int count = 0;
 
 void handle_sigint(int signo) {
     printf("\a");
-    fflush(NULL);
+    fflush(stdout);
     count++;
 }
 
 void handle_sigquit(int signo) {
     printf("\nThe signal sounded %d times.\n", count);
-    fflush(NULL);
+    fflush(stdout);
     exit(0);
 }
 
 int main(void) {
-    signal(SIGINT,  handle_sigint);
-    signal(SIGQUIT, handle_sigquit);
+    struct sigaction sa_int, sa_quit;
+
+    //SIGINT
+    sa_int.sa_handler = handle_sigint;
+    sigemptyset(&sa_int.sa_mask);
+    sa_int.sa_flags = 0;
+    sigaction(SIGINT, &sa_int, NULL);
+
+    //SIGQUIT
+    sa_quit.sa_handler = handle_sigquit;
+    sigemptyset(&sa_quit.sa_mask);
+    sa_quit.sa_flags = 0;
+    sigaction(SIGQUIT, &sa_quit, NULL);
 
     while (1)
         pause();
