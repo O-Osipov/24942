86479b1ee23ce05cfbccd8328f17c6f5bbb88fe2
Erbol Kubanychbek <e.kubanychbek@g.nsu.ru>
2025-11-19 14:28:06 +0700

    edit

--- e.kubanychbek/task25/25.c
+++ e.kubanychbek/task25/25.c
@@ -26,7 +26,7 @@ int main(void){
 
     //родительский процесс
     close(fd[0]); // закрываем конец для чтения 
-    const char *text = "Сжатие объектов: 100 (4/4), готово.\nЗапись объектов: 100 (5/5), 921 байт | 921.00 КиБ/с, готово.\nTotal 5 (delta 2), reused 0 (delta 0), pack-reused 0 (from 0)remote: \nResolving deltas: 100 (2/2), completed with 2 local objects.\nTo github.com:Erbol2006/24942.git1822a10..221bec5  main -> main\nThis is Mixed Case TeXT.\n";
+    const char *text = "Hello world, today I'm handing in assignments on operating systems.\n";
     write(fd[1], text, strlen(text));
     close(fd[1]); //закрываем pipe, то есть дочерний процеес увидит EOF
     return 0;
