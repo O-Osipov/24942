13d3ef3c44fac1b9255dcd63e9f8d2bd944348c7
Erbol Kubanychbek <e.kubanychbek@g.nsu.ru>
2025-11-26 13:58:21 +0700

    edit

--- e.kubanychbek/task17/17.c
+++ e.kubanychbek/task17/17.c
@@ -1,5 +1,4 @@
 #include <stdio.h> 
-//isprint - возращает true если это печатаемый ASCII символ
 #include <unistd.h> 
 #include <termios.h>
 #include <ctype.h>
@@ -8,42 +7,29 @@
 
 int main(void) {
     struct termios oldt, t;
-    //считаем текущие атрибуты терминала 
     tcgetattr(0, &oldt);
     t = oldt; 
 
-    //выключаем канонический режим и эхо
     t.c_lflag &= ~(ICANON | ECHO);
-
-    //читать по одному байту
     t.c_cc[VMIN] = 1; 
     t.c_cc[VTIME] = 0; 
-
-    //применяем новые настройки 
     tcsetattr(0, TCSANOW, &t);
 
     char line[MAXLEN + 1];
-    int len = 0; //текущая длина строки 
-    int col = 0; //номер колонки в строке  
-
+    int len = 0;  
+    int col = 0; 
     while (1) {
         char c; 
-        /*
-        Raw mode — это режим работы терминала, в котором ядро перестаёт обрабатывать ввод,
-        а программа получает символы напрямую, один за другим, без какой-либо интерпретации, 
-        задержки и редактирования.
-        */
-        read(0, &c, 1); //читаем один символ (в raw mode)
-
+        read(0, &c, 1); 
 
         //CTRL-D - завершение 
-        if (c == 4 && len == 0){ //ctrlD и пустая строка
+        if (c == 4 && len == 0){ 
             write(1, "\n", 1);
             break;
         }
 
         //BACKSPACE /ERASE
-        if (c == 127 || c == 8) { //DEL || Backspace
+        if (c == 127 || c == 8) { 
             if (len > 0 ){
                 len--;
                 col--;
@@ -51,7 +37,7 @@ int main(void) {
             }
             continue;
         }
-                //KILL (Ctrl-U) 
+        //KILL (Ctrl-U)  стираем все со строки 
         if (c == 21) { 
             while (len > 0) {
                 write(1, "\b \b", 3);
@@ -62,13 +48,11 @@ int main(void) {
         }
         //CTRL-W удаляем последнее слово 
         if (c == 23){
-            //удалим пробелы справа
             while (len > 0 && isspace((unsigned char)line[len-1])){
                 write(1, "\b \b", 3);
                 len--; 
                 col--;
             }
-            //удаляем буквы слова
             while (len > 0 && !isspace((unsigned char)line[len-1])){
                 write(1, "\b \b", 3);
                 len--; 
@@ -76,28 +60,20 @@ int main(void) {
             }
             continue; 
         }
-        //печатаемый символ
         if (isprint((unsigned char)c)){
-            //перенос строки при достижении MAXLEN 
             if (col == MAXLEN){
                 write(1, "\n", 1);
                 len = 0;
                 col = 0; 
                 line[0] = '\0';    
-            }
-            //добавляем символ в буфер 
+            } 
             line[len++] = c; 
             col++; 
-
-            //НЕМЕДЛЕННО выводим
             write (1, &c, 1);
             continue;
         }
-        //все остальные -> звуковой сигнал 
         write(1, "\a", 1);
     }
-    //восстанавливаем режим терминала
     tcsetattr(0, TCSAFLUSH, &oldt);
-
     return 0; 
 }
