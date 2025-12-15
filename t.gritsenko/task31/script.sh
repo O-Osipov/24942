f4522e0b87cfca472cda6f9303c55b235f2b0fd6
LijnxArcher <lynx20030311@gmail.com>
2025-12-15 20:25:02 +0700

    script fix

--- t.gritsenko/task31/script.sh
+++ t.gritsenko/task31/script.sh
@@ -17,7 +17,7 @@ echo "=== Запуск клиентов ==="
 # Клиент 1: шлёт X по одному символу
 (
   while true; do
-    printf "xxxxx"
+    printf "i am sync client"
     sleep 0.02
   done
 ) | ./client &
@@ -27,7 +27,7 @@ CLIENT1_PID=$!
 # Клиент 2: шлёт Y по одному символу
 (
   while true; do
-    printf "yyyyy"
+    printf "me sync client too"
     sleep 0.02
   done
 ) | ./client &
